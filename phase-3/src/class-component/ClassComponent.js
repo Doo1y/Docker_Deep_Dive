85eb0c3b1caceb09ff62241e86de4675d9781923
Samuel Park <s.park111497@gmail.com>
2023-01-29 13:59:30 -0800

    completed all 3 phases

---
+++ phase-3/src/class-component/ClassComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class ClassComponent extends React.Component {
+	render() {
+		return <h2>I am a Class Component</h2>;
+	}
+}
---
+++ phase-3/src/css-modules/HeadingA.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './HeadingA.module.css';
+
+function HeadingA() {
+	return <h2 className={styles.heading}>Welcome Home</h2>;
+}
+
+export default HeadingA;
